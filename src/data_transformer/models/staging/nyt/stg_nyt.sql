46e5bcaec5e6161d8a7735a98afc95978668d6c9
johnnyb1694 <johnnyb1694@gmail.com>
2024-12-21 11:03:56 +0000

    [Update]: (src/data_transformer) changed the name of the source to reflect NYT archive search (nytas)

--- src/data_transformer/models/staging/nyt/stg_nyt.sql
+++ src/data_transformer/models/staging/nyt/stg_nyt.sql
@@ -1,22 +1,22 @@
-with versioned_headlines as (
+with versioned_nytas as (
 
     select
         row_number() over (partition by url order by publication_date desc) as headline_version,
         *
-    from {{ source("src_nyt", "nyt") }}
+    from {{ source('src_nyt', 'nytas') }}
 
 ),
 
 final as (
 
     select
-        {{ dbt_utils.generate_surrogate_key(["headline", "url"]) }} as headline_id,
+        {{ dbt_utils.generate_surrogate_key(['headline', 'url']) }} as headline_id,
         headline,
         publication_date::DATE,
         coalesce(author, 'Unknown') as author,
         coalesce(news_desk, 'Unknown') as news_desk,
         url
-    from versioned_headlines
+    from versioned_nytas
     where headline_version = 1
     and headline is not null
 
